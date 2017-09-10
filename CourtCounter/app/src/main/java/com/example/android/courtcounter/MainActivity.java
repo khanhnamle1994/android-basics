27a185a799c909a213ef6cd18f46d87ad2e1120f
James Le <le_j6@denison.edu>
2017-09-10 16:56:04 +0700

    Setting Up The Methods

--- CourtCounter/app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ CourtCounter/app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.android.courtcounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void addThreeForTeamA(View v) {
+        displayForTeamA(3);
+    }
+
+    public void addTwoForTeamA(View v) {
+        displayForTeamA(2);
+    }
+
+    public void addOneForTeamA(View v) {
+        displayForTeamA(1);
+    }
+
+    /**
+     * Displays the given score for Team A.
+     */
+    public void displayForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
     }
 }
