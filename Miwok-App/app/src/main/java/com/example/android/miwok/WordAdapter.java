d31107ee5d8e592ac9fc839a7be1dfe839917e1f
James Le <le_j6@denison.edu>
2017-10-01 16:48:49 +0700

    Remaining Words

--- Miwok-App/app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ Miwok-App/app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -18,7 +18,7 @@ import java.util.ArrayList;
 
 public class WordAdapter extends ArrayAdapter<Word> {
 
-    public WordAdapter(Activity context, ArrayList<Word> androidFlavors) {
+    public WordAdapter(Activity context, ArrayList<Word> words) {
         // Here, we initialize the ArrayAdapter's internal storage for the context and the list.
         // the second argument is used when the ArrayAdapter is populating a single TextView.
         // Because this is a custom adapter for two TextViews and an ImageView, the adapter is not
