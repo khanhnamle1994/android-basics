fcdd597eab48dcaef416963ba796cbae82c890a6
James Le <le_j6@denison.edu>
2017-10-01 12:51:29 +0700

    Build List Item Layout

--- Miwok-App/app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ Miwok-App/app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -3,7 +3,10 @@ package com.example.android.miwok;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -28,29 +31,10 @@ public class NumbersActivity extends AppCompatActivity {
         words.add("nine");
         words.add("ten");
 
-        // Find the root view so we can add child views to it
-        LinearLayout rootView = (LinearLayout)findViewById(R.id.rootView);
+        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, words);
 
-        // Create a variable to keep track of the current index position
-        int index = 0;
+        ListView listView = (ListView) findViewById(R.id.list);
 
-        // Keep looping until we've reached the end of the list (which means keep looping
-        // as long as the current index position is less than the length of the list).
-        // The index variable keeps track of our current position in the list and
-        // increments by 1 each time the code in the loop is executed.
-
-        for (int index = 0; index < words.size(); index++) {
-            // Create a new TextView
-            TextView wordView = new TextView(this);
-
-            // Set the text to be word at the current index
-            wordView.setText(words.get(index));
-
-            // Add this TextView as another child to the root view of this layout
-            rootView.addView(wordView);
-
-            // Increment the index variable by 1
-            index++;
-        }
+        listView.setAdapter(itemsAdapter);
     }
 }
