22bd068f179581f0dd4d1236403387660451d83d
James Le <le_j6@denison.edu>
2017-10-13 07:15:05 +0700

    Display date and time of earthquakes

--- QuakeReport/app/src/main/java/com/example/android/quakereport/Earthquake.java
+++ QuakeReport/app/src/main/java/com/example/android/quakereport/Earthquake.java
@@ -1,5 +1,7 @@
 package com.example.android.quakereport;
 
+import static com.example.android.quakereport.R.id.date;
+
 /**
  * An {@link Earthquake} object contains information related to a single earthquake.
  */
@@ -12,20 +14,20 @@ public class Earthquake {
     /** Location of the earthquake */
     private String mLocation;
 
-    /** Date of the earthquake */
-    private String mDate;
+    /** Time of the earthquake */
+    private long mTimeInMilliseconds;
 
     /**
      * Constructs a new {@link Earthquake} object.
      *
      * @param magnitude is the magnitude (size) of the earthquake
      * @param location is the city location of the earthquake
-     * @param date is the date the earthquake happened
+     * @param timeInMilliseconds is the time in milliseconds (from the Epoch) when the earthquake happened
      */
-    public Earthquake(String magnitude, String location, String date) {
+    public Earthquake(String magnitude, String location, long timeInMilliseconds) {
         mMagnitude = magnitude;
         mLocation = location;
-        mDate = date;
+        mTimeInMilliseconds = timeInMilliseconds;;
     }
 
     /**
@@ -41,5 +43,5 @@ public class Earthquake {
     /**
      * Returns the date of the earthquake.
      */
-    public String getDate() { return mDate; }
+    public long getTimeInMilliseconds() { return mTimeInMilliseconds; }
 }
